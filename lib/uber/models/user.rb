8b3781efccce97360d36c8c418faa932ce74d6b7
arunthampi <arun.thampi@gmail.com>
2015-06-15 11:46:59 -0700

    Add spec for /v1/me

--- lib/uber/models/user.rb
+++ lib/uber/models/user.rb
@@ -1,5 +1,5 @@
 module Uber
   class User < Base
-    attr_accessor :first_name, :last_name, :email, :picture, :promo_code
+    attr_accessor :first_name, :last_name, :email, :picture, :promo_code, :uuid
   end
 end
