93e95463d8d2a008c4d601eab1691c13954453fd
Christopher EnyTC <chris@enytc.com>
2016-07-26 17:05:06 -0300

    Update request model to handle single errors

--- lib/uber/models/request.rb
+++ lib/uber/models/request.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
 module Uber
   class Request < Base
-    attr_accessor :request_id, :status, :vehicle, :driver, :location, :eta, :surge_multiplier, :meta, :errors
+    attr_accessor :request_id,
+                  :status,
+                  :vehicle,
+                  :driver,
+                  :location,
+                  :eta,
+                  :surge_multiplier,
+                  :meta,
+                  :errors
 
     def driver=(value)
       @driver = value.nil? ? nil : Driver.new(value)
@@ -14,8 +23,16 @@ module Uber
       @location = value.nil? ? nil : Location.new(value)
     end
 
+    def error=(value)
+      @error = value.nil? ? nil : RequestError.new(value)
+    end
+
     def errors=(values)
       @errors = values.map { |v| RequestError.new(v) }
+    end
+
+    def errors?
+      @errors && @errors.size >= 1 || @error && @error.key?("code")
     end
 
     def vehicle_name
@@ -23,22 +40,20 @@ module Uber
     end
 
     def vehicle_image_url
-      vehicle.nil? ? nil : "#{vehicle.picture_url}"
+      vehicle.nil? ? nil : vehicle.picture_url.to_s
     end
 
     def driver_image_url
-      driver.nil? ? nil : "#{driver.picture_url}"
+      driver.nil? ? nil : driver.picture_url.to_s
     end
 
     def humanized_eta
-      unless eta.nil?
-        eta.to_i == 1 ? "#{eta} minute" : "#{eta} minutes"
-      end
+      !eta.nil? && eta.to_i == 1 ? "#{eta} minute" : "#{eta} minutes"
     end
   end
 
   class RequestError < Base
-    attr_accessor :status, :code, :title
+    attr_accessor :status, :code, :title, :message
   end
 
   class Driver < Base
