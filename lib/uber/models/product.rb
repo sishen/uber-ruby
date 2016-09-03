c1ddfbdf91c248ccbd27dbca436725a228edca46
Ankur Goel <ankurgel@gmail.com>
2016-09-03 18:26:28 +0530

    expore more available options in Product

--- lib/uber/models/product.rb
+++ lib/uber/models/product.rb
@@ -1,5 +1,6 @@
 module Uber
   class Product < Base
-    attr_accessor :product_id, :description, :display_name, :capacity, :image
+    attr_accessor :product_id, :description, :display_name, :capacity, :image,
+                  :cash_enabled, :shared, :price_details
   end
 end
