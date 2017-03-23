3e03d8b08b763dbdd280bfe80ff61ce1293d91e9
Anh Pham <anh@hoteltonight.com>
2017-03-22 18:32:19 -0700

    Upgrade to v1.2/me

--- lib/uber/models/user.rb
+++ lib/uber/models/user.rb
@@ -1,6 +1,6 @@
 module Uber
   class User < Base
-    attr_accessor :first_name, :last_name, :email, :picture, :promo_code, :mobile_verified, :uuid
+    attr_accessor :first_name, :last_name, :email, :picture, :promo_code, :mobile_verified, :uuid, :rider_id
 
     def mobile_verified?
       !!self.mobile_verified
