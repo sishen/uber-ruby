51f20988959853cbde473469f0d8e5a205a619bd
Ankur Goel <ankurgel@gmail.com>
2017-10-27 08:53:41 +0530

    add new delivery

--- lib/uber/api/deliveries.rb
+++ lib/uber/api/deliveries.rb
@@ -12,6 +12,11 @@ module Uber
         perform_with_objects(:get, "/v1/deliveries", arguments.options, Delivery::Delivery)
       end
 
+      def add_delivery(*args)
+        arguments = Uber::Arguments.new(args)
+        perform_with_object(:post, "/v1/deliveries", arguments.options, Delivery::Delivery)
+      end
+
       def delivery(delivery_id)
         perform_with_object(:get, "/v1/deliveries/#{delivery_id}", {}, Delivery::Delivery)
       end
