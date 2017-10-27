5a0c793095075041e82166f59c249fc311b3f705
Ankur Goel <ankurgel@gmail.com>
2017-10-27 08:21:22 +0530

    serve GET /deliveries/:delivery_id

--- lib/uber/api/deliveries.rb
+++ lib/uber/api/deliveries.rb
@@ -11,6 +11,10 @@ module Uber
         arguments = Uber::Arguments.new(args)
         perform_with_objects(:get, "/v1/deliveries", arguments.options, Delivery::Delivery)
       end
+
+      def delivery(delivery_id)
+        perform_with_object(:get, "/v1/deliveries/#{delivery_id}", {}, Delivery::Delivery)
+      end
     end
   end
 end
